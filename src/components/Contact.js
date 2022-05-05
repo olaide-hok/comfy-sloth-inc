38b6cf94128334c40c2be28a24c29c8f128f8d4e
olaide-hok <olaidekareem43@yahoo.com>
2022-05-05 16:48:29 +0100

    Add Formspree action link to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </p>
+          <form
+            className="contact-form"
+            action="https://formspree.io/f/mqkngvqz"
+            method="POST"
+          >
+            <input
+              type="email"
+              name="email"
+              className="form-input"
+              placeholder="enter email"
+            />
+            <button type="submit" className="submit-btn">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +94,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
