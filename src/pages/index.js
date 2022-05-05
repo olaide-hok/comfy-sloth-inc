20591b6a03c0bdc2377c93e2564f2bfd40142926
olaide-hok <olaidekareem43@yahoo.com>
2022-05-05 12:21:59 +0100

    Create Navbar, Footer component.

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Error from './ErrorPage'
+
+export {
+    Home,
+    About,
+    Cart,
+    Checkout,
+    PrivateRoute,
+    Products,
+    SingleProduct,
+    Error
+}
