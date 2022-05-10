933f3be1fc4bec1173e010bc3bea6c4d9d02f0cc
olaide-hok <olaidekareem43@yahoo.com>
2022-05-10 16:00:33 +0100

    Complete Stripe Checkout Feature

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -27,7 +27,7 @@ const CheckoutForm = () => {
   const [processing, setProcessing] = useState('');
   const [disabled, setDisabled] = useState(true);
   const [clientSecret, setClientSecret] = useState('')
-  const stripe = useState()
+  const stripe = useStripe()
   const elements = useElements()
 
   const cardStyle = {
@@ -55,8 +55,6 @@ const CheckoutForm = () => {
         JSON.stringify({cart, shipping_fee, total_amount})
       )
       setClientSecret(data.clientSecret)
-      console.log(data.clientSecret);
-
     } catch (error) {
       console.log(error.response);      
     }
@@ -68,15 +66,47 @@ const CheckoutForm = () => {
   }, [])
 
   const handleChange = async (event) => {
-
+    setDisabled(event.empty)
+    setError(event.error ? event.error.message : '')
   }
 
   const handleSubmit = async (ev) => {
-    
+    ev.preventDefault()  
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement)
+      }
+    })
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false)
+    } else {
+      setError(null)
+      setProcessing(false)
+      setSucceeded(true)
+      setTimeout(() => {
+        clearCart()
+        history.push('/')
+      }, 10000)
+    }
+
   }
 
 
   return <div>
+    {
+      succeeded ? <article>
+        <h4>Thank you</h4>
+        <h4>Your payment was successful!</h4>
+        <h4>Redirecting to home page shortly...</h4>
+      </article> :
+      <article>
+        <h4>Hello, {myUser && myUser.name}</h4>
+        <p>Your total is {formatPrice(shipping_fee + total_amount)} </p>
+        <p>Test Card Number : 4242 4242 4242 4242</p>
+      </article>
+    }
     <form id='payment-form' onSubmit={handleSubmit}>
       <CardElement
         id='card-element'
